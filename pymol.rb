90cd3aad21394c1281e8be106d75b3044b1ea357
Derek Thomas <derekathomas@gmail.com>
2012-12-13 08:52:29 +0900

    Add test for pymol

--- pymol.rb
+++ pymol.rb
@@ -57,6 +57,6 @@ class Pymol < Formula
         # This test will fail and we won't accept that! It's enough to just replace
         # "false" with the main program this formula installs, but it'd be nice if you
         # were more thorough. Run the test with `brew test pymol`.
-        system "false"
+        system "pymol", "-c"
     end
 end
